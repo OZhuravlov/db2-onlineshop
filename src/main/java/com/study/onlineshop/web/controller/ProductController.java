e0c82c15dec06b73bf05179118c14ba1b54bf253
Oleksandr Zhuravlov <alex.shadoof@gmail.com>
2018-11-15 12:10:39 +0200

    migrating to ThymeLeaf init

--- src/main/java/com/study/onlineshop/web/controller/ProductController.java
+++ src/main/java/com/study/onlineshop/web/controller/ProductController.java
@@ -8,10 +8,8 @@ import com.study.onlineshop.service.ProductService;
 import com.study.onlineshop.web.templater.PageGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,14 +25,10 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    private PageGenerator pageGenerator = PageGenerator.instance();
-
     @RequestMapping(value = {"/products", "/"}, method = RequestMethod.GET)
-    @ResponseBody
-    public String getAll(HttpServletRequest request) throws IOException {
+    public String getAll(HttpServletRequest request, Model model) throws IOException {
         List<Product> products = productService.getAll();
-        HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("products", products);
+        model.addAttribute("products", products);
         Session session = (Session) request.getAttribute("session");
         int cartCount = 0;
         if (session != null) {
@@ -43,68 +37,58 @@ public class ProductController {
                 cartCount = cart.getProducts().size();
             }
         }
-        parameters.put("cartCount", cartCount);
-
+        model.addAttribute("cartCount", cartCount);
         boolean editMode = session.getUser().getUserRole().equals(UserRole.ADMIN);
-        parameters.put("editMode", editMode);
+        model.addAttribute("editMode", editMode);
 
-        String page = pageGenerator.getPage("products", parameters);
-        return page;
+        return "products";
     }
 
     @RequestMapping(path = "/product/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String getByIdPath(@PathVariable int id) throws IOException {
+    public String getByIdPath(@PathVariable int id, Model model) throws IOException {
         List<Product> products = Arrays.asList(productService.getById(id));
-        HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("products", products);
-        String page = pageGenerator.getPage("products", parameters);
-        return page;
+        model.addAttribute("products", products);
+        return "products";
     }
 
     @RequestMapping(path = "/product/add", method = RequestMethod.POST)
-    public void addProduct(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
-        double price = Double.parseDouble(request.getParameter("price"));
-        Product product = new Product();
-        product.setName(name);
-        product.setPrice(price);
+    public String addProduct(HttpServletRequest request
+            , HttpServletResponse response
+            , @ModelAttribute Product product) throws IOException {
         product.setCreationDate(LocalDateTime.now());
         int id = productService.add(product);
         product.setId(id);
-        response.sendRedirect("/products");
+        return "redirect:/products";
     }
 
 
     @RequestMapping(path = "/product/edit/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String getEditProduct(@PathVariable int id) throws IOException {
-        PageGenerator pageGenerator = PageGenerator.instance();
+    public String getEditProduct(@PathVariable int id, Model model) throws IOException {
         Product product = productService.getById(id);
-
         HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("product", product);
-        String page = pageGenerator.getPage("product-edit", parameters);
-        return page;
+        model.addAttribute("product", product);
+        return "product-edit";
     }
 
 
     @RequestMapping(path = "/product/edit/{id}", method = RequestMethod.POST)
-    public void editProduct(HttpServletRequest request, HttpServletResponse response, @PathVariable int id) throws IOException {
+    public String editProduct(HttpServletRequest request, HttpServletResponse response
+            , @PathVariable int id) throws IOException {
         String name = request.getParameter("name");
         double price = Double.parseDouble(request.getParameter("price"));
         if (name == null || name.isEmpty() || price <= 0d) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return null;
         } else {
             productService.update(id, name, price);
-            response.sendRedirect("/products");
+            return ("redirect:/products");
         }
     }
 
     @RequestMapping(path = "/product/delete/{id}", method = RequestMethod.POST)
-    public void deleteProduct(HttpServletRequest request, HttpServletResponse response, @PathVariable int id) throws IOException {
+    public String deleteProduct(HttpServletRequest request, HttpServletResponse response, @PathVariable int id) throws IOException {
         productService.delete(id);
-        response.sendRedirect("/products");
+        return "redirect:/products";
     }
 
     public void setProductService(ProductService productService) {
